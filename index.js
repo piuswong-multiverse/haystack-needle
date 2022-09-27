3efa63ce156565df387760e907abe34a183a9ea3
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-09-27 16:55:27 -0500

    Initial commit for main function

---
+++ index.js
@@ -0,0 +1,14 @@
+// YOUR CODE
+const myIncludes = (haystack, needle) => {
+    let found = false;
+
+    console.log(found);
+    return found;
+}
+
+myIncludes('carwash','c');
+
+
+// DO NOT EDIT BELOW
+
+module.exports = myIncludes;
