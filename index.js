a607d6f5bbb6ec12091fa0ab5f2f23a16b7a30e1
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-10-03 18:36:18 -0500

    Passes tests to find multi-char strings in 'carwash'

--- index.js
+++ index.js
@@ -13,21 +13,32 @@ const myIncludes = (haystack, needle) => {
                 if(needle[0]===haystack[j]){
                     return true
                 }
+            } else if(needle.length>1){
+                // console.log(i,j,needle[i],haystack[indexFoundInHaystack + i]) // debug
+
+                // If searching for multiple characters, first find the starting index in the haystack where it's found.
+                if(needle[i]===haystack[j]){
+                    if(indexFoundInHaystack===null && i===0){
+                        indexFoundInHaystack = j;
+                        break;    
+                        // Then check if each next character matches, stopping if at the end of the needle.
+                    } else if(indexFoundInHaystack!==null && needle[i]===haystack[indexFoundInHaystack + i]){
+                        if(i===(needle.length-1)){
+                            return true;
+                        }
+                        break;
+                    }
+                }
             }
 
-            // // If 1st character of the needle is found, search for the rest
-            // if(needle[0]===haystack[j]){
-            //     indexFoundInHaystack = j;
-            //     break 
-            // } else {
-            //     return false;
-            // }
+            
         }
-        return found;
     }
 
+    // By default return that it's not found; only returns true if meeting conditions above.
+    return found;
 }
 
-
+myIncludes("carwash", "wash");
 
 module.exports = myIncludes;
