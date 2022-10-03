30840edb5133a8efc693dbbd2d1940d93ac7661f
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-10-03 17:21:39 -0500

    Can find single characters in string

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-// YOUR CODE
 const myIncludes = (haystack, needle) => {
-    let found = false;
 
-    console.log(found);
-    return found;
+    let found = false;
+    let indexFoundInHaystack = null;
+
+    // Go through each character in the needle, finding it in the haystack in order.
+    // Avoid using premade JS search functions, to practice using loops.
+    for(let i=0; i<needle.length; i++){
+        for(let j=0; j<haystack.length; j++){
+
+            // Stop early if searching for just one character
+            if(needle.length===1){
+                if(needle[0]===haystack[j]){
+                    return true
+                }
+            }
+
+            // // If 1st character of the needle is found, search for the rest
+            // if(needle[0]===haystack[j]){
+            //     indexFoundInHaystack = j;
+            //     break 
+            // } else {
+            //     return false;
+            // }
+        }
+        return found;
+    }
+
 }
 
 
-// DO NOT EDIT BELOW
 
 module.exports = myIncludes;
--- index.test.js
+++ index.test.js
@@ -1,10 +1,13 @@
 const myIncludes = require('./index');
 
+
 describe('Finds single characters in haystack', () => {
 
     test('Find letters in carwash', () => {
-        let needle = "c";
         let haystack = "carwash";
+        let needle = "c";
+        expect(myIncludes(haystack,needle)).toEqual(true);
+        needle = "a";
         expect(myIncludes(haystack,needle)).toEqual(true);
     })
 
