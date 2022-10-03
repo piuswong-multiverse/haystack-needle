26d1a1bffaa9da2ec27cec03c5603128fd621059
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-10-03 17:26:13 -0500

    Finds single characters; pass and fail cases tested

--- index.test.js
+++ index.test.js
@@ -1,16 +1,32 @@
 const myIncludes = require('./index');
 
 
-describe('Finds single characters in haystack', () => {
+describe('how well it finds single characters in haystack', () => {
 
-    test('Find letters in carwash', () => {
+    it('finds single letters in "carwash"', () => {
         let haystack = "carwash";
         let needle = "c";
         expect(myIncludes(haystack,needle)).toEqual(true);
         needle = "a";
         expect(myIncludes(haystack,needle)).toEqual(true);
+        needle = "b";
+        expect(myIncludes(haystack,needle)).toEqual(false);
     })
 
-    
+    it('finds characters in a random sentence', () => {
+        let haystack = "Mrs. Smith's daily goal was to improve on yesterday.";
+        let needle = "M";
+        expect(myIncludes(haystack,needle)).toEqual(true);
+        needle = "m";
+        expect(myIncludes(haystack,needle)).toEqual(true);
+        needle = ".";
+        expect(myIncludes(haystack,needle)).toEqual(true);
+        needle = "y";
+        expect(myIncludes(haystack,needle)).toEqual(true);
+        needle = "q";
+        expect(myIncludes(haystack,needle)).toEqual(false);
+        needle = ":";
+        expect(myIncludes(haystack,needle)).toEqual(false);
+    })
 
 })
