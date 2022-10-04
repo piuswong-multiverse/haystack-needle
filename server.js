4b55a3a29b9e749dd56a56fe34cca7fd2855e60d
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-10-04 18:13:21 -0500

    Front-end form structure exists, backend API skeleton exists

--- server.js
+++ server.js
@@ -6,8 +6,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json()); // Allow getting POST data from req.body
+
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname,'index.html'));
+})
+
+app.post('/api', async (req,res) => {
+    try{
+        const data = await req.body;
+        // TODO: get haystack and needle data, and run myIncludes...
+        res.send(`TEXT GOES HERE`)
+    } catch (err) {
+        res.send({message: err})
+    }
 })
 
 app.listen(port, () => {
